7595fc99eb02bb9fd90f50394a0a1c25570b3efe
Guy-Michel Destin <destin.guymichel@hotmail.fr>
2016-09-16 14:48:29 +0200

    correction bug date ajout plusieur event en cours

--- www/js/controllers/HomeController.js
+++ www/js/controllers/HomeController.js
@@ -1,22 +1,25 @@
 app.controller('HomeController', ['$scope', '$cordovaCalendar','NotificationsService',function($scope, $cordovaCalendar, NotificationsService){
     NotificationsService.GetNotifications().then(function(notifications){
         $scope.notifications = notifications;
-    });
-
-
-    $scope.createEvent = function() {
-        $cordovaCalendar.createEventInteractively({
-            title:  $scope.notifications[1].name,
+      return notifications;
+    }).then(function(notifs){
+      console.log(notifs);
+      for(var i in notifs) {
+        console.log(notifs[i]);
+        $scope.createEvent = function () {
+          $cordovaCalendar.createEventInteractively({
+            title: notifs[i].name,
             location: 'Itechsup',
-            notes:  $scope.notifications[1].description,
-            startDate: new Date($scope.notifications.timestart*1000),
-            endDate: new Date(2016, 08, 17, 12, 0, 0, 0, 0)
-        }).then(function (result) {
+            notes: notifs[i].description,
+            startDate: new Date((notifs[i].timestart) * 1000),
+            endDate: new Date(((notifs[i].timestart) * 1000) + ((notifs[i].timeduration) * 1000))
+          })
+        }
+      }
+    }).then(function (result) {
             console.log("Event created successfully");
-            console.log($stateParams.notification);
+            //console.log($stateParams.notification);
         }, function (err) {
             console.error("There was an error: " + err);
         });
-
-    }
 }]);
